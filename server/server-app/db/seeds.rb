672e835500e8f6f815eb22d6fd22fb24d89dfbb7
WNomunomu <kohsei.nmr@gmail.com>
2024-07-02 15:18:50 +0000

    implement code for creating test data

--- server/server-app/db/seeds.rb
+++ server/server-app/db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Creating todos...'
+
+5.times do |i|
+  Todo.create(title: "Todo#{i + 1}", completed: false)
+end
+
+puts '...Finished!'
